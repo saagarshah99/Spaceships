1efb23f6fb3094c0f0a6f840562ccbf579e1cd32
Saagar Shah <saagarshahs@hotmail.com>
2021-08-26 17:13:00 +0100

    Refactoring JS

--- assets/scripts/spaceships.js
+++ assets/scripts/spaceships.js
@@ -1,78 +1,85 @@
-//////////// COLLISION ALGO FROM MDN
-/* 
-var circle1 = {radius: 20, x: 5, y: 5};
-var circle2 = {radius: 12, x: 10, y: 5};
-
-var dx = circle1.x - circle2.x;
-var dy = circle1.y - circle2.y;
-var distance = Math.sqrt(dx * dx + dy * dy);
-
-if (distance < circle1.radius + circle2.radius) {
-    // collision detected!
-}
-*/
-
-// https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+//generate a random hash colour sequence
+const randomColour = () => "#"+Math.floor(Math.random()*16777215).toString(16);
 
 const collider = 
 {
-    moveableDiv: null,
-    staticDivs: [],
-    checkCollision: function() 
+    spaceship: null, starDivs: [],
+    
+    // looping to compare position of spaceship with stars to check for collisions
+    // checkCollision: function() 
+    checkCollision() 
     {
         let hasJustCollided = false;            
-        const staticClass = document.querySelectorAll(".static");
-        for (let i = 0; i < this.staticDivs.length; i++) 
+        const starClass = document.querySelectorAll(".stars");
+        for (let i = 0; i < this.starDivs.length; i++) 
         {
+            const currentStar = this.starDivs[i];
+
+            const dx = currentStar.position.left - this.spaceship.position.left;
+            const dy = currentStar.position.top - this.spaceship.position.top;
             
-            const currentDiv = this.staticDivs[i];
-            const dx = currentDiv.position.left - this.moveableDiv.position.left;
-            const dy = currentDiv.position.top - this.moveableDiv.position.top;
-            const distance = Math.sqrt(dx*dx + dy*dy);
-            if(distance < currentDiv.position.radius + this.moveableDiv.position.radius) 
+            //if distance between the current star and spaceship coordinates < their combined radius 
+            if(Math.sqrt(dx*dx + dy*dy) < currentStar.position.radius+this.spaceship.position.radius) 
             {
                 hasJustCollided = true;
+                starClass[i].classList.add("hidden");
 
-                if (!this.moveableDiv.ref.classList.contains('collision-state')) 
+                if(!this.spaceship.ref.classList.contains('collision-state')) 
                 {
-                    this.moveableDiv.ref.classList.add('collision-state');
-                    staticClass[i].classList.add("hide-me");
-                    
+                    this.spaceship.ref.classList.add('collision-state');
                 }
             } 
-            else if (this.moveableDiv.ref.classList.contains('collision-state') && !hasJustCollided) 
+            else if(this.spaceship.ref.classList.contains('collision-state') && !hasJustCollided) 
             {
-                this.moveableDiv.ref.classList.remove('collision-state');
+                this.spaceship.ref.classList.remove('collision-state');
             }
-
         }
 
-        const hideMeClass = document.querySelectorAll(".hide-me");
-        // if(staticClass.length == hideMeClass.length) setup();
-        if(staticClass.length == hideMeClass.length) createRandomDivs();
+        // TODO: this only runs twice, length eventually gets uneven
+        if(starClass.length == document.querySelectorAll(".hidden").length) createRandomStars();
     },
 };
 
+// generate specific amount of randomly positions stars
+const createRandomStars = () =>
+{
+    for (let i = 0; i < 1; i++) 
+    {
+        const newStarDiv = document.createElement('div');
+        
+        newStarDiv.setAttribute('style', `left: ${Math.floor(Math.random() * 400)}px; top: ${Math.floor(Math.random() * 600)}px; background-color: ${randomColour()};`);
+
+        document.querySelector('.space-container').appendChild(newStarDiv);
+        newStarDiv.classList.add("collidable-object", "stars");
+        collider.starDivs.push(new BaseDiv(newStarDiv));
+    }
+}
 
 // the prototype for my base div
-window.BaseDiv = function(div) 
+window.BaseDiv = function(star) 
 {
     this.position = 
     {
-        left: div.getBoundingClientRect().left,
-        top: div.getBoundingClientRect().top,
-        radius: div.getBoundingClientRect().height / 2,
+        left: star.getBoundingClientRect().left,
+        top: star.getBoundingClientRect().top,
+        radius: star.getBoundingClientRect().height / 2,
     };
 }
 
-
-window.MoveDiv = function(ref) 
+/***
+ * .prototype allows you to add new properties/functions to object constructors
+ * receiving keyboard input and appropriately updating current position
+ */
+window.MoveStar = function(ref) {this.ref = ref; BaseDiv.call(this, ref);}
+MoveStar.prototype.shiftPosition = function(x, y) 
 {
-    this.ref = ref;
-    BaseDiv.call(this, ref);
+    this.position.left += x; 
+    this.position.top += y;
+    
+    this.ref.setAttribute('style', `left: ${this.position.left}px; top: ${this.position.top}px`);
+    collider.checkCollision();
 }
-
-MoveDiv.prototype.moveOnKeyPress = function(e) 
+MoveStar.prototype.moveOnKeyPress = function(e) 
 {
     switch(e.code) 
     {            
@@ -80,63 +87,26 @@ MoveDiv.prototype.moveOnKeyPress = function(e)
         case "KeyA": case "ArrowLeft": this.shiftPosition(-5, 0); break;
         case "KeyS": case "ArrowDown": this.shiftPosition(0, 5); break;
         case "KeyD": case "ArrowRight": this.shiftPosition(5, 0); break;
-        default: break;
+        
+        default: break; //ignore other keyboard input
     }
 }
 
-MoveDiv.prototype.shiftPosition = function(x, y) 
+// setting up entire game "canvas" and listening for keyboard input on page load
+const setup = () =>
 {
-    this.position.left += x;
-    this.position.top += y;
-    this.reDraw(this.ref, this.position);
-}
-
-MoveDiv.prototype.reDraw = function(ref, position) 
-{
-    ref.setAttribute('style', `left: ${position.left}px; top: ${position.top}px`);
-    collider.checkCollision();
-}
-
-function setup() 
-{
-    const container = document.querySelector('.container');
-    if(document.querySelector("#divtwo")) 
-    {
-        document.querySelector("#divtwo").remove();
-    }
+    const spaceship = document.querySelector("#spaceship");    
+    if(spaceship) spaceship.remove(); //remove previous spaceship in new game
+    
     // TODO: speed/velocity needs to be reset as it gets incremently faster
     
-    // create a bunch of random divs
-    createRandomDivs();
-    
-    // create the moveable div
-    const newMoveableDiv = document.createElement('div');
-    // newMoveableDiv.setAttribute('style', 'left: 500px; top: 500px;');
-    newMoveableDiv.setAttribute('id', 'divtwo');
-    newMoveableDiv.classList.add('collideme');
-    container.appendChild(newMoveableDiv);
-    collider.moveableDiv = new MoveDiv(newMoveableDiv);
-
-    document.addEventListener('keydown', (e) => collider.moveableDiv.moveOnKeyPress(e));
-};
-setup();
-
-function createRandomDivs()
-{
-    const container = document.querySelector('.container');
-
-    for (let i = 0; i < 5; i++) 
-    {
-        const newStaticDiv = document.createElement('div');
-        newStaticDiv.setAttribute('style', `left: ${Math.floor(Math.random() * 400)}px; top: ${Math.floor(Math.random() * 600)}px; background-color: ${generateRandomColour()};`);
-        container.appendChild(newStaticDiv);
-        newStaticDiv.classList.add("collideme", "static");
-        collider.staticDivs.push(new BaseDiv(newStaticDiv));
-    }
+    // creating random stars and new spaceship, then positioning them
+    createRandomStars();
+    const newSpaceship = document.createElement('div');
+    newSpaceship.setAttribute('style', 'left: 500px; top: 500px;');
+    newSpaceship.setAttribute('id', 'spaceship');
+    newSpaceship.classList.add('collidable-object');
+    document.querySelector('.space-container').appendChild(newSpaceship);
+    collider.spaceship = new MoveStar(newSpaceship);    
 }
-
-//generate a random hash colour sequence
-function generateRandomColour()
-{
-    return "#"+Math.floor(Math.random()*16777215).toString(16);
-}
+setup(); document.addEventListener('keydown', (e) => collider.spaceship.moveOnKeyPress(e));
