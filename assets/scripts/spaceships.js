635ce4465b62bb9036f91cd6b631924907f12ea8
Saagar Shah <saagarshahs@hotmail.com>
2021-08-31 14:45:46 +0100

    Removing confetti canvas once explosion complete to improve mouse input accuracy

--- assets/scripts/spaceships.js
+++ assets/scripts/spaceships.js
@@ -30,9 +30,13 @@ const collisionObject = {
 
         // explode confetti from left/right & generate new stars/planets when current set runs out
         if(starClass.length == document.querySelectorAll(".space__hidden").length) {
-            confetti.create(document.querySelector(".explode"), { resize: true });
+            const canvas = document.createElement("canvas")
+            
+            confetti.create(canvas.classList.add("explode"), { resize: true });
             confetti({particleCount: 100, angle: 60, spread: 55, origin: { x: 0 }});
             confetti({particleCount: 100, angle: 120, spread: 55, origin: { x: 1 }});
+            
+            canvas.remove();
             
             createRandomStars(document.querySelector(".space__config__txtStars"));
         }
