9051389b768b633ff04ec7c33af1a7951fa562a6
saagarshah99 <saagarshahs@hotmail.com>
2021-09-04 23:24:22 +0100

    Generate small white circles every 5 planets to represent stars

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,23 @@
 /*****************************SPAWNING/STYLING SPACESHIP & STARS*****************************/
 
 // generating width, position (coordinates) and colour for current new star
-const addStyleToStar = (newStarDiv) => {
-    newStarDiv.style.width = newStarDiv.style.height = randomSize();
+const addStyleToStar = (newStarDiv, i) => {
+    let colour = randomColour();
+    let size = randomSize();
     
-    newStarDiv.style.top = randomPosition("vh"); 
-    newStarDiv.style.bottom = randomPosition("vh"); 
+    //small white circles every 5 planets to represent stars
+    if(i % 5 === 0) {
+        colour = "white";
+        size = "20px";
+    } 
+    
+    newStarDiv.style.backgroundColor = colour;
+    newStarDiv.style.width = newStarDiv.style.height = size;
+    
+    newStarDiv.style.top = randomPosition("vh");
+    newStarDiv.style.bottom = randomPosition("vh");
     newStarDiv.style.left = randomPosition("vw");
     newStarDiv.style.right = randomPosition("vw");
-    
-    newStarDiv.style.backgroundColor = randomColour();
 }
 
 // generate specific amount of stars w/ random sizes, positions and colours
@@ -20,7 +28,7 @@ const createRandomStars = () => {
 
     for (let i = 0; i < txtNumberOfStars.value; i++) {
         const newStarDiv = document.createElement("div");
-        addStyleToStar(newStarDiv);
+        addStyleToStar(newStarDiv, i);
 
         document.querySelector(".space").appendChild(newStarDiv);
         newStarDiv.classList.add("space__collidable-object", "space__stars");
